a5c4dcd92bc06d9cf87392ead686b115161693d2
Ernest <epospiky@gmail.com>
2023-03-10 04:31:00 +0000

    updated code, created new classes, added report

---
+++ road_traffic_simulation/src/road_traffic_simulation/Vehicle_manager.java
@@ -0,0 +1,17 @@
+package road_traffic_simulation;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Vehicle_manager {
+    private List<Vehicle> vehicles;
+    
+    public Vehicle_manager() {
+        vehicles = new ArrayList<>();
+    }
+    
+
+}
